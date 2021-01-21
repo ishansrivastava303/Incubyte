34c27317eae9581f7d63fb07cb8d909b051b0ce8
Ishan Srivastava <I24338@verisk.com>
2021-01-21 19:47:31 +0530

    removed commented lines

--- StringCalculator/Numbers.java
+++ StringCalculator/Numbers.java
@@ -44,40 +44,4 @@ public class Numbers {
 		}
 		return sum;
 	}
-	/*
-	public void findNegativeNumbers(String inputString)
-	{
-		findNegSignIndices(inputString);
-		String number="";
-		for(int i=0;i<negNumberIndices.size();i++)
-		{
-			if(negNumberIndices.get(i)==inputString.length()-1)
-				break;
-			number="";
-			if(Character.isDigit(inputString.charAt(negNumberIndices.get(i)+1)))
-			{
-				for(int j=negNumberIndices.get(i)+1;j<inputString.length();j++)
-				{
-					if(Character.isDigit(inputString.charAt(j)))
-						number+=String.valueOf(inputString.charAt(j));
-					else
-						break;
-				}
-				negNumbers.add(Integer.parseInt(number)*(-1));
-			}
-		}
-		System.out.println("negNumbers:"+negNumbers);
-	}
-	
-	public void findNegSignIndices(String inputString)
-	{
-		int index=inputString.indexOf("\n");
-		int negSignIndex=inputString.indexOf("-", index+1);
-		while(negSignIndex>=0)
-		{
-			negNumberIndices.add(negSignIndex);
-			negSignIndex=inputString.indexOf("-",negSignIndex+1);
-		}
-	}*/
-	
 }
